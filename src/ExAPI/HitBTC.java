f2202412a1998e8f36a1de0209366b49f4510f42
root <root@Kapuca-HP450G4>
2018-02-08 03:34:08 +0100

    Getting balance from HitBTC.

--- src/ExAPI/HitBTC.java
+++ src/ExAPI/HitBTC.java
@@ -1,5 +1,82 @@
 package ExAPI;
 
-public class HitBTC {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import com.google.gson.Gson;
+
+import structures.*;
+
+public class HitBTC extends ApiAccount{
+
+	public HitBTC(String username, String password) {
+		super(username, password);
+		this.ApiUrl= "https://api.hitbtc.com/api/2/";
+	}
+	
+	public HitBTC() {
+		this(null, null);
+	}
+
+	public Balance[] getTradingBalance() throws ConnectionException {
+		
+		HttpsURLConnection conn= makeConn("trading/balance");
+
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(conn.getInputStream())
+			);
+			//System.out.println("Output from Server ....");
+
+			String output = br.readLine();
+			Gson gson= new Gson();
+			Balance[] response= gson.fromJson(output, Balance[].class);
+			
+			conn.disconnect();
+			
+			return response;
+			
+		} catch (IOException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public Balance[] getAccountBalance() throws ConnectionException {
+		
+		HttpsURLConnection conn= makeConn("account/balance");
+
+		try {
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(conn.getInputStream())
+			);
+			//System.out.println("Output from Server ....");
+
+			String output = br.readLine();
+			Gson gson= new Gson();
+			Balance[] response= gson.fromJson(output, Balance[].class);
+			
+			conn.disconnect();
+			
+			return response;
+			
+		} catch (IOException e) {
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		return null;
+		
+	}
+	@Override
+	public Balance[] getBalance() throws ConnectionException {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+	
+	
 }
--- src/Shara.java
+++ src/Shara.java
@@ -1,52 +1,31 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import com.google.gson.Gson;
+import structures.*;
+
+import ExAPI.*;
 
 public class Shara {
 	public static void main(String[] args) {
 
-		  try {
 
-			  
-			URL url = new URL("https://api.hitbtc.com/api/2/public/symbol");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-
-			if (conn.getResponseCode() != 200) {
-				throw new RuntimeException("Failed : HTTP error code : "
-						+ conn.getResponseCode());
+		HitBTC account= new HitBTC("userpass here", "secret here");
+		Balance[] arr;
+		try {
+			arr = account.getAccountBalance();
+			
+			for(int i=0; i< arr.length; i++){
+				if (arr[i].available!=0 || arr[i].reserved!=0) {
+					arr[i].Info();					
+				}
 			}
-
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-				(conn.getInputStream())));
-
-			String output = br.readLine();
-			System.out.println("Output from Server ....");
-			Gson gson= new Gson();
-			Symbol[] symbols= gson.fromJson(output, Symbol[].class);
-			for(int i=0; i< symbols.length; i++){
-				symbols[i].Info();
-			}
-
-			conn.disconnect();
-
-		  } catch (MalformedURLException e) {
-
+		} catch (ConnectionException e) {
+			System.err.println(e.getMessage());
+			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
 
-		  } catch (IOException e) {
-
-			e.printStackTrace();
-
-		  }
 		  
 
-		}
+	}
 
 
 }
